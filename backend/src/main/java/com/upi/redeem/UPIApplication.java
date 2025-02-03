c8f761dd646a735f3506565a70b2f8ca6abee3a1
sanuksd <100213849+sanuksd@users.noreply.github.com>
2025-02-03 20:44:04 +0530

    Add files via upload

---
+++ backend/src/main/java/com/upi/redeem/UPIApplication.java
@@ -0,0 +1,11 @@
+package com.upi.redeem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UPIApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UPIApplication.class, args);
+    }
+}
